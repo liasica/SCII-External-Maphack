4f1dff23fdd49ea22ca5a1386a39a470f2afb319
kekehaa <kekehaa@gmail.com>
2012-03-26 20:14:53 -0700

    hudmanager started - new method of keeping hud windows in their place
    Signed-off-by: kekehaa <kekehaa@gmail.com>

---
+++ SC2 External Maphack_Source/screen-utilities/hudManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ini;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace maphack_external_directx.screen_utilities
+{
+    class hudManager
+    {
+        // screen info
+        public static int resolutionHeight = 0;
+        public static int resolutionWidth = 0;
+        public static int numOfScreens = 0;
+        
+        public void screenInfo()
+        {
+            if (resolutionHeight == 0 && resolutionWidth == 0 && numOfScreens == 0)
+            {
+                Screen[] screens = Screen.AllScreens;
+                resolutionWidth  = Screen.PrimaryScreen.Bounds.Width;
+                resolutionHeight = Screen.PrimaryScreen.Bounds.Height;
+                numOfScreens = screens.Count();
+            }
+        }
+
+
+
+
+
+    }
+}
